4df5b100250652a0dec2ba55e6eecd1cb7456173
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-08-24 23:49:57 -0500

    Upload all files just at the end of the day. This is a general message.

--- functions/src/users/domain/usecases/get-teams-that-i-belong/get-teams-that-i-belong.usecase.ts
+++ functions/src/users/domain/usecases/get-teams-that-i-belong/get-teams-that-i-belong.usecase.ts
@@ -10,8 +10,10 @@ export class GetTeamsThatIBelongUsecase extends Usecase<string, any> {
           name: 'Once Caldas A',
           category: 'Sub 15',
           id: 'aT83kzfkTA1V81EyXNgk',
-          miniShield: 'https://firebasestorage.googleapis.com/v0/b/deporty-dev.appspot.com/o/teams%2FaT83kzfkTA1V81EyXNgk%2Fbrand%2Fshield.png?alt=media&token=58ba4343-c8f4-49ce-bfc7-7d3b2c042445',
-          shield: 'https://firebasestorage.googleapis.com/v0/b/deporty-dev.appspot.com/o/teams%2FaT83kzfkTA1V81EyXNgk%2Fbrand%2Fshield.png?alt=media&token=58ba4343-c8f4-49ce-bfc7-7d3b2c042445',
+          miniShield:
+            'https://firebasestorage.googleapis.com/v0/b/deporty-dev.appspot.com/o/teams%2FaT83kzfkTA1V81EyXNgk%2Fbrand%2Fshield.png?alt=media&token=58ba4343-c8f4-49ce-bfc7-7d3b2c042445',
+          shield:
+            'https://firebasestorage.googleapis.com/v0/b/deporty-dev.appspot.com/o/teams%2FaT83kzfkTA1V81EyXNgk%2Fbrand%2Fshield.png?alt=media&token=58ba4343-c8f4-49ce-bfc7-7d3b2c042445',
         } as TeamEntity,
         member: {
           id: '590ae4eb023b40debb36',
