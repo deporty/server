6f657987057cdc82a5bf3a49a6d1c1ebba3748f5
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-09-26 19:08:37 -0500

    Upload all files just at the end of the day. This is a general message.

--- servers/users/src/domain/usecases/team-participations/delete-team-participation/delete-team-participation.usecase.ts
+++ servers/users/src/domain/usecases/team-participations/delete-team-participation/delete-team-participation.usecase.ts
@@ -6,6 +6,7 @@ import { map, mergeMap } from 'rxjs/operators';
 import { GetTeamsThatIBelongUsecase } from '../get-teams-that-i-belong/get-teams-that-i-belong.usecase';
 import { GetUserByIdUsecase } from '../../get-user-by-id/get-user-by-id.usecase';
 import { DeleteUserUsecase } from '../../delete-user/delete-user.usecase';
+import { DEFAULT_OWNER_ROLES_ID } from '../../../../infrastructure/users.constants';
 
 export interface Param {
   userId: string;
@@ -67,28 +68,22 @@ export class DeleteTeamParticipationUsecase extends Usecase<Param, boolean> {
           if (h.length === 0) {
             return this.getUserByIdUsecase.call(param.userId).pipe(
               map((user: UserEntity) => {
-                return user.administrationWay === 'delegated' || true;
+                const kind = user.roles.filter((R) => {
+                  return DEFAULT_OWNER_ROLES_ID.includes(R);
+                });
+
+                return (user.administrationWay === 'delegated' || true) && kind.length == 0;
               })
             );
           }
           return of(false);
         }),
         mergeMap((toDelete: boolean) => {
-          return of(toDelete);
           if (toDelete) {
-            return of(false)
             return this.deleteUserUsecase.call(param.userId);
           }
           return of(true);
         })
       );
-    // .pipe(
-    //   map((id) => {
-    //     return {
-    //       ...param,
-    //       id,
-    //     };
-    //   })
-    // );
   }
 }
--- servers/users/src/infrastructure/users.constants.ts
+++ servers/users/src/infrastructure/users.constants.ts
@@ -2,6 +2,7 @@ export const USERS_ENTITY = 'users';
 export const TEAM_PARTICIPATIONS_ENTITY = 'teams-participations';
 export const SERVER_NAME = 'USER';
 
+export const DEFAULT_OWNER_ROLES_ID = ['0c4ea7ed93c74d5bb226', 'h66hpX3eGT7F1BH3BXDG'];
 
 // export const SERVER = 'https://us-central1-deporty-dev.cloudfunctions.net'
 export const SERVER = 'http://127.0.0.1:10000';
@@ -9,5 +10,4 @@ export const SERVER = 'http://127.0.0.1:10000';
 export const AUTHORIZATION_SERVER = `${SERVER}/authorization`;
 export const TEAM_SERVER = `${SERVER}/teams`;
 
-export const BEARER_TOKEN = 'f599e916-841b-4a1b-aa0a-65fefcaadf09'
-
+export const BEARER_TOKEN = 'f599e916-841b-4a1b-aa0a-65fefcaadf09';
