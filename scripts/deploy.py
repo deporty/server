4719b265f4c0d4d0dfabcd3b7a9003f16fabd538
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-10-08 22:19:16 -0500

    Upload all files just at the end of the day. This is a general message.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -27,14 +27,14 @@ with open('kubernetes.config.json', 'r') as f:
     KUBERNETES_CONFIGURATION = json.load(f)
 
 servers_to_deploy = extract_servers_to_deploy(files)
-servers_to_deploy = [
-    ('authorization','1.0.1'),
-    ('users','1.0.3'),
-    ('teams','1.0.2'),
-    ('tournaments','1.0.2'),
-    ('organizations','1.0.1'),
-    ('locations','1.0.1')
-]
+# servers_to_deploy = [
+#     ('authorization','1.0.1'),
+#     ('users','1.0.3'),
+#     ('teams','1.0.2'),
+#     ('tournaments','1.0.2'),
+#     ('organizations','1.0.1'),
+#     ('locations','1.0.1')
+# ]
 
 env = os.environ.get('ENV') or 'pdn'
 
--- servers/authorization/scripts/build.py
+++ servers/authorization/scripts/build.py
@@ -1,8 +1,37 @@
-import os
 import shutil
 import subprocess    
-res = subprocess.check_output('cd .. && npx tsc -p tsconfig.json',  shell=True, text=True)
+import argparse
 
+parser = argparse.ArgumentParser(description="")
+    
+# Agrega argumentos
+parser.add_argument('--env', help="Environtment: local, dev, pdn")
+
+# Parsea los argumentos
+args = parser.parse_args()
+
+env = args.env or 'pdn'  
+
+current_env_path  = f'../environments/env.ts'
+target_env_path  = f'../environments/{env}.ts'
+
+with open(current_env_path, 'r') as t:
+    current_env_content = t.read()
+
+with open(target_env_path, 'r') as t:
+    target_env_content = t.read()
+
+with open(current_env_path, 'w') as t:
+    t.write(target_env_content)
+
+
+
+
+res = subprocess.check_output('cd .. && npx tsc -p tsconfig.json',  shell=True, text=True)
+  
+
+with open(current_env_path, 'w') as t:
+    t.write(current_env_content)
 
 files = [
     {
