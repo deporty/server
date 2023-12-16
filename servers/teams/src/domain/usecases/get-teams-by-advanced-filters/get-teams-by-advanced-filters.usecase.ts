0dc22c6f71339e5d18c97ca9b1485c8fd7d53068
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-12-15 23:30:20 -0500

    Upload all files just at the end of the day. This is a general message.

--- servers/teams/src/domain/usecases/get-teams-by-advanced-filters/get-teams-by-advanced-filters.usecase.ts
+++ servers/teams/src/domain/usecases/get-teams-by-advanced-filters/get-teams-by-advanced-filters.usecase.ts
@@ -1,13 +1,20 @@
 import { TeamEntity } from '@deporty-org/entities';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TeamContract } from '../../contracts/team.contract';
 import { Usecase } from '@scifamek-open-source/iraca/domain';
+import { catchError } from 'rxjs/operators';
 
 export class GetTeamByAdvancedFiltersUsecase extends Usecase<any, Array<TeamEntity>> {
   constructor(private teamContract: TeamContract) {
     super();
   }
   call(filters: any): Observable<Array<TeamEntity>> {
-    return this.teamContract.filter(filters);
+    return this.teamContract.filter(filters).pipe(
+      catchError((data) => {
+        console.log(data, "Los datacios");
+        
+        return of([]);
+      })
+    );
   }
 }
